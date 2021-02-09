86edbfa743ca1e69b1fd2eb6bf544d1cf8ba4686
riley powell <rileypowell1@hotmail.co.uk>
2021-02-09 11:46:52 +1100

    added buildspec

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {   
+    
+    parser:"babel-eslint",
+
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "jquery": {
+            "globals": {
+                $: false
+            }
+        }
+    
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended"
+    ],
+    
+    "parserOptions": {
+        "ecmaFeatures": {
+            jsx: true,
+            globalReturn: false,
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module",
+        "allowImportExportEverywhere": false,
+        "babelOptions": {
+            configFile: "babel.config.json",
+       },
+    },
+
+    "plugins": [
+        "react",
+        "eslint-plugin-import",
+        "eslint-plugin-jsx-a11y",
+        "eslint-plugin-react"
+    ],
+
+    "rules": {
+        "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+        "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+        semi: "error",
+                "myPlugin/my-rule": "error",
+                "eslint-plugin-myPlugin/another-rule": "error"
+    }
+};
